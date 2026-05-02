d961a25e58b546246677571096766d5699675cc1
VericaG <georgievskaverica9@gmail.com>
2026-05-02 19:45:11 +0200

    One service for all info functions from uslugi.gov institutions

--- institutions/uslugi/service_registry.py
+++ institutions/uslugi/service_registry.py
@@ -1,14 +1,40 @@
 # institutions/uslugi/service_registry.py
 
 SERVICES = {
-    5084: "misdemeanor_sanction_certificate_individual",
-    4467: "independent_artist_registration",
-    5026: "single_member_llc_registration",
-    5240: "misdemeanor_sanction_certificate_company",
-    5253: "pledge_registry_information_certificate",
-    5254: "leasing_registry_information_certificate",
-    5298: "pledge_right_deletion",
-    5299: "leasing_deletion",
-    5304: "llc_company_registration",
-    5305: "sole_trader_registration",
+
+    # Centralen registar na RSM
+    5084: "misdemeanor_cert_individual",
+    4467: "independent_artist_reg",
+    5026: "single_member_llc_reg",
+    5240: "misdemeanor_cert_company",
+    5253: "pledge_registry_cert",
+    5254: "leasing_registry_cert",
+    5298: "pledge_delete",
+    5299: "leasing_delete",
+    5304: "llc_company_reg",
+    5305: "sole_trader_reg",
+
+    # Ministerstvo za trud i socijalna politika
+    5099: "min_guaranteed_assistance",
+    5102: "reduced_hours_allowance",
+    5106: "elderly_social_security",
+    5108: "education_allowance",
+    5109: "third_child_allowance",
+    5454: "child_allowance",
+    5455: "special_allowance",
+    5456: "education_allowance_usage",
+    5471: "permanent_allowance",
+    5472: "disability_allowance",
+
+    # Regulatorna komisija
+    5463: "electricity_transmission_license",
+    5469: "gas_transmission_license",
+    5476: "energy_license_transfer",
+    5477: "energy_license_termination",
+    5478: "electricity_market_operator_license",
+    5481: "gas_market_operator_license",
+    5482: "gas_distribution_license",
+    5485: "temporary_heat_production_license",
+    5486: "heat_production_temp_holder",
+    5487: "heat_distribution_license",
 }
